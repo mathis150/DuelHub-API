6ca41a8aac1e34159ea35d3ed2c9e0da8a664551
ElKariboosama <tsunaris123456789@gmail.com>
2023-11-16 15:44:17 +0100

    change: require to import, add: error middleware

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 //Imports required
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const OpenApiValidator = require('express-openapi-validator')
-const swaggerUI = require('swagger-ui-express')
-const YALM = require('yamljs')
+import express from "express"
+import cors from "cors"
+import OpenApiValidator from "express-openapi-validator"
+import swaggerUI from 'swagger-ui-express'
+import YAML from "yamljs"
+
+export const app = express()
+
 
 //Fichier Swagger
-const swaggerDocument = YALM.load('./open-api.yaml')
+const swaggerDocument = YAML.load('./open-api.yaml')
 
 //Middlewares globaux
 app.use(cors())
@@ -21,5 +23,15 @@ app.use(
     })
 )
 
+app.get("/testconnection",(req,res) => {
+    res.status(200).json({code:200,message:"successfully connected to database"})
+})
+
+app.use((err,req,res,next)=> {
+    err.status = err.status || 500
+    err.message = err.message || "no error message"
+    res.status(err.status).json(err)
+}) 
+
+
 //Export pour server.js
-module.exports = app
