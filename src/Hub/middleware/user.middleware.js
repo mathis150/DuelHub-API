7a4925ba64573be6b1cd5cd4ffc958d8552c522f
ElKariboosama <tsunaris123456789@gmail.com>
2023-12-04 08:29:48 +0100

    fix status message

--- src/Hub/middleware/user.middleware.js
+++ src/Hub/middleware/user.middleware.js
@@ -6,11 +6,11 @@ export const useruuidcheck = (req,res,next) => {
 }
 
 export const frienduuidcheck = (req,res,next) => {
-    if(!isUUID(req.params.uuid_friend)) next({code:400,status:"provided for parameter \"uuid_user\" is not a uuid"})
+    if(!isUUID(req.params.uuid_friend)) next({code:400,status:"provided for parameter \"uuid_friend\" is not a uuid"})
     next()
 }
 
 export const favoriteuuidcheck = (req,res,next) => {
-    if(!isUUID(req.params.uuid_favorite)) next({code:400,status:"provided for parameter \"uuid_user\" is not a uuid"})
+    if(!isUUID(req.params.uuid_favorite)) next({code:400,status:"provided for parameter \"uuid_favorite\" is not a uuid"})
     next()
 }
