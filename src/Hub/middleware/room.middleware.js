7e6ace3df676582a5745ce169281117d3c126969
ElKariboosama <tsunaris123456789@gmail.com>
2024-01-03 09:07:02 +0100

    update middleware

--- src/Hub/middleware/room.middleware.js
+++ src/Hub/middleware/room.middleware.js
@@ -1,4 +1,4 @@
-import { isUUID } from "../utils/uuid.utils.js"
+import { isUUID } from "../utils/regex.util.js"
 
 export const roomuuidcheck = (req,res,next) => {
     if(!isUUID(req.params.uuid_room)) next({code:400,status:"provided for parameter \"uuid_room\" is not a uuid"})
@@ -6,13 +6,20 @@ export const roomuuidcheck = (req,res,next) => {
 }
 
 export const roombodycheck = (req,res,next) => {
-    //TODO: implement check for a standardised room
+    var errorflag = false
+
+    if(!req.body.uuid_user) errorflag = true
+    if(!req.body.title) errorflag = true
+
+    if(errorflag) next({code:400,status:"one or more of the given information empty if you do not use a field set it to null"})
+    next()
 }
 
-export const depthuuidcheck = (req,res,next) => {
+export const offsetuuidcheck = (req,res,next) => {
     if(!isUUID(req.params.depth)) next({code:400,status:"provided for parameter \"depth\" is not a uuid"})
     next()
 }
+
 
 export const newnamecheck = (req,res,next) => {
     if(!isUUID(req.params.new_name)) next({code:400,status:"provided for parameter \"new_name\" is not a uuid"})
