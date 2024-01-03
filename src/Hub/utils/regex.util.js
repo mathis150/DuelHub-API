362a681bcc426e1b23182fa14ec5f003824ec78a
ElKariboosama <tsunaris123456789@gmail.com>
2024-01-03 09:08:21 +0100

    add utils

---
+++ src/Hub/utils/regex.util.js
@@ -0,0 +1,7 @@
+export const isUUID = (string) => {
+    return /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/.test(string)
+}
+
+export const isEmail = (string) => {
+    return /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(string)
+}
