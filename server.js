8d51a3a9d2c82c3910f646ee9721ec8768a8ca69
ElKariboosama <tsunaris123456789@gmail.com>
2024-01-03 09:09:29 +0100

    add route

--- server.js
+++ server.js
@@ -2,20 +2,21 @@ import dotenv from "dotenv"
 dotenv.config()
 
 import { app } from "./app.js"
-import { userrouter } from "./src/Hub/routers/user.router.js"
+import * as authmiddleware from "./src/Hub/middleware/auth.middleware.js"
+import * as userrouter from "./src/Hub/routers/user.router.js"
+import * as gamerouter from "./src/Hub/routers/game.router.js"
+import * as roomrouter from "./src/Hub/routers/room.router.js"
+import * as messagerouter from "./src/Hub/routers/message.router.js"
 
 const port = process.env.SERVERPORT || 3000
 
-app.use("/user",userrouter)
+app.use("/user",userrouter) //public
+app.use("/game",gamerouter) //public
+app.use("/room",roomrouter) //private
+app.use("/message",messagerouter) //private
 
 app.use("/",(req,res) => {
     res.json({code:200,message:"connection successful"})
-})
-
-app.use((err,req,res,next) => {
-    err.code = err.code || 500
-    err.status = err.status || "no error message provided"
-    res.json({code:err.code,status:err.status})
 })
 
 app.listen(port, () => {
