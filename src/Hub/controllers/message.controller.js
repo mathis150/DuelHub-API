bce679c4442808bdefc3f61c26567c932f20f9ef
ElKariboosama <tsunaris123456789@gmail.com>
2024-01-03 09:06:54 +0100

    update controller

---
+++ src/Hub/controllers/message.controller.js
@@ -0,0 +1,25 @@
+import * as service from "../services/message.service.js"
+
+export const getmessage = (req,res) => {
+    res.json(service.getmessage(req.params.uuid_message))
+}
+
+export const getmessagedetails = (req,res) => {
+    res.json(service.getmessagedetails(req.params.uuid_message))
+}
+
+export const getmessagreply = (req,res) => {
+    res.json(service.getreply(req.params.uuid_message))
+}
+
+export const addmessage = (req,res) => {
+    res.json(service.addmessage(req.body.uuid_room,req.body.uuid_user,req.body.content,req.body.uuid_reply))
+}
+
+export const modifymessage = (req,res) => {
+    res.json(service.editmessage(req.params.uuid_message,req.body.content))
+}
+
+export const deletemessage = (req,res) => {
+    res.json(service.addmessage(req.params.uuid_message))
+}
--- src/Hub/controllers/user.controller.js
+++ src/Hub/controllers/user.controller.js
@@ -1,5 +1,76 @@
+import * as service from '../services/user.service.js'
+
+//? GET
+
+export const getuser = (req,res) => {
+    res.json(service.getuser(req.params.uuid_user))
+}
+
+export const getuserdetails = (req,res) => {
+    res.json(service.getuserdetails(req.params.uuid_user))
+}
+
+export const getuserfriendlist = (req,res) => {
+    res.json(service.getuserfriendlist(req.params.uuid_user))
+}
+
+export const getuserfavoritelist = (req,res) => {
+    res.json(service.getuserfavoritelist(req.params.uuid_user))   
+}
+
+export const getusergamelist = (req,res) => {
+    res.json(service.getusergamelist(req.params.uuid_user)  )  
+}
+
+export const getuserroomlist = (req,res) => {
+    res.json(service.getuserroomlist(req.params.uuid_user))
+}
+
+export const getuserbyusername = (req,res) => {
+    res.json(service.getuserbyusername(req.params.username))
+}
 
 
-export const getuserinfo = () => {
-    
+//? POST
+
+export const createuser = (req,res) => {
+    res.json(service.adduser(req.body.username,req.body.password,req.body.email))
+}
+
+export const addfavorite = (req,res) => {
+    res.json(service.addfavorite(req.params.uuid_user,req.params.uuid_favorite))
+}
+
+export const addfriend = (req,res) => {
+    res.json(service.addfriend(req.params.uuid_user,req.params.uuid_friend))
+}
+
+export const addgame = (req,res) => {
+    res.json(service.addgame(req.params.uuid_user,req.params.uuid_game))
+}
+
+export const addroom = (req,res) => {
+    res.json(service.addroom(req.params.uuid_user,req.params.uuid_room))
+}
+
+//? DELETE
+
+export const deleteuser = (req,res) => {
+    res.json(service.deleteuser(req.params.uuid_user))
+}
+
+export const removefavorite = (req,res) => {
+    res.json(service.removefavorite(req.params.uuid_user,req.params.uuid_favorite))
+}
+
+export const removefriend = (req,res) => {
+    res.json(service.removefriend(req.params.uuid_user,req.params.uuid_friend))
+}
+
+export const removegame = (req,res) => {
+    res.json(service.removegame(req.params.uuid_user,req.params.uuid_game))
+}
+
+export const removeroom = (req,res) => {
+    res.json(service.removeroom(req.params.uuid_user,req.params.uuid_room))
 }
